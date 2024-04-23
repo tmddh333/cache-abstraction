438f6c3d7258499496c743c8073d2d249c61f4f7
백승오 <tmdh3333@fasoo.com>
2024-04-23 17:21:48 +0900

    initial commit

---
+++ src/main/java/com/example/cacheabstraction/CacheAbstractionApplication.java
@@ -0,0 +1,13 @@
+package com.example.cacheabstraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheAbstractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheAbstractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cacheabstraction/CacheAbstractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cacheabstraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheAbstractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
